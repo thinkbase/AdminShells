1b60bb6b96c04b972f297da9957240a21eba1972
thinkbase <thinkbase.net@gmail.com>
2013-04-16 23:24:31 +0800

    1. timestamp.bat - add TIMESTAMP_DATE support; 2. wmic-ascii.bat - echo the command line before execution;

--- .includes/timestamp.bat
+++ .includes/timestamp.bat
@@ -2,3 +2,4 @@
 :: FIXME: depends on the OS version and timezone/locale setting
 ::set TIMESTAMP=%date:~0,4%-%date:~5,2%-%date:~8,2%.%time:~0,2%-%time:~3,2%-%time:~6,5%
 set TIMESTAMP=%DATE:/=-%@%TIME::=-%
+set TIMESTAMP_DATE=%DATE:/=-%
--- wmic/wmic-ascii.bat
+++ wmic/wmic-ascii.bat
@@ -6,6 +6,7 @@ SETLOCAL
 call %~dp0..\.includes\timestamp.bat
 
 set TMP_WMIC_OUT=%TEMP%\wmic-output-%TIMESTAMP%-%RANDOM%.txt
+echo wmic %*
 wmic /output:"%TMP_WMIC_OUT%" %*
 cmd /A /C type "%TMP_WMIC_OUT%"
 del "%TMP_WMIC_OUT%"
