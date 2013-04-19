e2456b1d7dd0565bcb6065210ecd2593bc614d1c
thinkbase <thinkbase.net@gmail.com>
2013-04-20 03:14:27 +0800

    Output format update

--- wmic/wmic-ascii.bat
+++ wmic/wmic-ascii.bat
@@ -6,9 +6,12 @@ SETLOCAL
 call %~dp0..\.includes\timestamp.bat
 
 set TMP_WMIC_OUT=%TEMP%\wmic-output-%TIMESTAMP%-%RANDOM%.txt
-echo wmic %*
+echo ^>^>^> wmic %*
 wmic /output:"%TMP_WMIC_OUT%" %*
 cmd /A /C type "%TMP_WMIC_OUT%"
 del "%TMP_WMIC_OUT%"
+
+echo.
+echo.
 
 ENDLOCAL
