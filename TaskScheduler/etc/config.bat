7d4524ceb855d39079695c7ef5da1abe0c575294
thinkbase <thinkbase.net@gmail.com>
2013-09-18 19:06:10 -0700

    Trim the heading BLANK of BATCH_SHELL_LOG_TIME , becuase the time string like " 3:12:09" should cause env variable BATCH_SHELL_LOG_TIME not defined error

--- TaskScheduler/etc/config.bat
+++ TaskScheduler/etc/config.bat
@@ -5,7 +5,10 @@
 
 :: NOTE: The environment variables in this file should be overridden by %USERPROFILE%\@batch-shell-config.bat
 
-@set BATCH_SHELL_LOG_DIR=%~dp0%..\.logs
+@IF [] equ [%BATCH_SHELL_LOG_DIR%] set BATCH_SHELL_LOG_DIR=%~dp0%..\.logs
 :: depends on the OS version, DATE and TIME variable should be different, please make sure it's currect!
 @set BATCH_SHELL_LOG_DATE=%date:~0,4%-%date:~5,2%-%date:~8,2%
 @set BATCH_SHELL_LOG_TIME=%time:~0,2%-%time:~3,2%-%time:~6,5%
+:: replace the heading BLANK
+@set BATCH_SHELL_LOG_DATE=%BATCH_SHELL_LOG_DATE: =%
+@set BATCH_SHELL_LOG_TIME=%BATCH_SHELL_LOG_TIME: =%
