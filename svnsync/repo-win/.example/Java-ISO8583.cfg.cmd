05ea0010e6fdd5373c619cbd0ccb8eb0e67528e5
trac-zh <trac-zh@users.noreply.github.com>
2015-04-14 12:46:57 +0800

    Encoding ANSI, Windows EOL format

--- svnsync/repo-win/.example/Java-ISO8583.cfg.cmd
+++ svnsync/repo-win/.example/Java-ISO8583.cfg.cmd
@@ -1,4 +1,5 @@
-set _REPO_DIR=%SITE_BASE%\repo-win\Java-ISO8583
-set _SVN_URL=http://svn.code.sf.net/p/j8583/code/
-set _SVN_SYNC_U=anonymous
-set _SVN_SYNC_P=none
+:: Encoding: ANSI
+set _REPO_DIR=%SITE_BASE%\repo-win\Java-ISO8583
+set _SVN_URL=http://svn.code.sf.net/p/j8583/code/
+set _SVN_SYNC_U=anonymous
+set _SVN_SYNC_P=none
--- svnsync/repo-win/.example/jxplorer.cfg.cmd
+++ svnsync/repo-win/.example/jxplorer.cfg.cmd
@@ -1,4 +1,5 @@
-set _REPO_DIR=%SITE_BASE%\repo-win\jxplorer
-set _SVN_URL=svn://svn.code.sf.net/p/jxplorer/code
-set _SVN_SYNC_U=anonymous
-set _SVN_SYNC_P=none
+:: Encoding: ANSI
+set _REPO_DIR=%SITE_BASE%\repo-win\jxplorer
+set _SVN_URL=svn://svn.code.sf.net/p/jxplorer/code
+set _SVN_SYNC_U=anonymous
+set _SVN_SYNC_P=none
