fb6ef155413eb7ff28315a723be566a9ac861ba5
trac-zh <trac-zh@users.noreply.github.com>
2017-12-01 06:27:19 +0800

    Declare bat/cmd files that will always have CRLF line endings on checkout.

--- svnsync/svn-sync.bat
+++ svnsync/svn-sync.bat
@@ -8,7 +8,6 @@ set PATH=%SITE_BASE%\..\.bin\svn-win32-1.6.15\bin;%PATH%
 set LANG=en_US.UTF-8
 set LC_CTYPE=en_US.UTF-8
 
-
 pushd "%SITE_BASE%\repo-win"
 for %%i in (*.cfg.cmd) do (
     echo.
@@ -29,6 +28,11 @@ for %%i in (*.cfg.cmd) do (
         svnadmin create .
         echo ::It's blank > ./hooks/pre-revprop-change.bat
         svnsync initialize --non-interactive --source-username=!_SVN_SYNC_U! --source-password=!_SVN_SYNC_P! file:///!_REPO_URL! !_SVN_URL!
+			set _ERR_CODE=!ERRORLEVEL!
+			IF NOT "!_ERR_CODE!"=="0" (
+				echo ">>> svnsync initialize ERROR: !_ERR_CODE!"
+			    exit /b !_ERR_CODE!
+			)
         popd
     )
     echo !_REPO_URL!: begin to sync ...
@@ -40,6 +44,11 @@ for %%i in (*.cfg.cmd) do (
     svn propset svn:sync-from-url --revprop -r0 !_SVN_URL! file:///!_REPO_URL!
     REM Ö´ÐÐ SVN ¿âÍ¬²½
     svnsync sync --non-interactive --source-username=!_SVN_SYNC_U! --source-password=!_SVN_SYNC_P! file:///!_REPO_URL!
+		set _ERR_CODE=!ERRORLEVEL!
+		IF NOT "!_ERR_CODE!"=="0" (
+			echo ">>> svnsync sync ERROR: !_ERR_CODE!"
+			exit /b !_ERR_CODE!
+		)
     echo Finish: !date! !time! *******
 )
 popd
